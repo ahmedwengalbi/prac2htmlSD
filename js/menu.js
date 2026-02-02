d1da7f26e98271545a3f68f15a5f8a30836372c3
daniilsamusev <psina0102@gmail.com>
2026-02-02 23:11:24 +0500

    Delete js directory

--- js/menu.js
+++
@@ -1,6 +0,0 @@
-const btn = document.getElementById("gamesBtn");
-const menu = document.getElementById("gamesMenu");
-
-btn.addEventListener("click", () => {
-    menu.classList.toggle("hidden");
-});
